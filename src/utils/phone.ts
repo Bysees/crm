7e6b4dca94406429ade93219ad6fe0cd9d0fb0dc
Bysees <bysees33@gmail.com>
2023-02-27 01:25:41 +0300

    fix phone number transform

--- src/utils/phone.ts
+++ src/utils/phone.ts
@@ -2,7 +2,7 @@
 //* На нормальном номере телефона она не сработает.
 
 export const transformPhone = (phone: string) => {
-  if (phone.startsWith('79') && phone.length === 9) {
+  if (phone.startsWith('7') && phone.length === 9) {
     const [n1, n2, n3, n4, n5, n6, n7, n8, n9] = phone.split('')
 
     return `+${n1} (${n2}${n3}${n4}) ${n5}${n6}${n7}-${n8}${n9}-**`
